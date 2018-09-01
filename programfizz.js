3928d533b0753cdd479e12f5d2f756b9acee3eb5
Humera97 <humeraslodhi@gmail.com>
2018-08-31 22:22:54 -0500

    Created fizzbuzz function

---
+++ programfizz.js
@@ -0,0 +1,17 @@
+function fizzbuzz() {
+	for (i=1, i<100, i++) {
+		if (i%3 == 0 && i%5 == 0) {
+			print("Fizz buzz");
+			}
+		else if (i%3 == 0) {
+			print("Fizz");
+			}
+		else if (i%5 == 0) {
+			print("buzz");
+			}
+		else {
+			print(i)'
+			}
+		}
+fizzbuzz()
+	
